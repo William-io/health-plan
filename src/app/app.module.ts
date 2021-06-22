c92dfa56fc4634e5a0a3f816f9ef31bad36c43f9
William <capuletos@live.com>
2021-06-22 17:14:02 -0300

    frame.page.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { LoginPageComponent } from './pages/account/login-page/login-page.compon
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
 import { ConsultPageOnlineComponent } from './pages/account/consult-page-online/consult-page-online.component';
-import { ConsultPhysicalComponent } from './pages/account/store/consult-physical/consult-physical.component';
-import { CartPageComponent } from './pages/account/store/cart-page/cart-page.component';
+import { ConsultPhysicalComponent } from './pages/store/consult-physical/consult-physical.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 
 
 @NgModule({
