7cb84e5adf9bc2359a52676d357d49cd235fb333
William <capuletos@live.com>
2021-06-22 16:16:44 -0300

    Organização do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { ConsultPageOnlineComponent } from './consult-page-online/consult-page-online.component';
-import { ConsultPhysicalComponent } from './consult-physical/consult-physical.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { ConsultPageOnlineComponent } from './pages/account/consult-page-online/consult-page-online.component';
+import { ConsultPhysicalComponent } from './pages/account/store/consult-physical/consult-physical.component';
+import { CartPageComponent } from './pages/account/store/cart-page/cart-page.component';
 
 
 @NgModule({
--- src/app/pages/account/reset-password-page/reset-password-page.component.ts
+++ src/app/pages/account/reset-password-page/reset-password-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reset-password-page',
   templateUrl: './reset-password-page.component.html',
-  styleUrls: ['./reset-password-page.component.css']
+  // styleUrls: ['./reset-password-page.component.css']
 })
 export class ResetPasswordPageComponent implements OnInit {
 
